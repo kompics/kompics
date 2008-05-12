e079cf117913950b8262b337091057bcc3f07dd8
Cosmin Arad <cosmin@sics.se>
2008-05-12 10:28:42 +0000

    added component registry and membrane

--- core/src/main/java/se/sics/kompics/api/ComponentMembrane.java
+++ core/src/main/java/se/sics/kompics/api/ComponentMembrane.java
@@ -6,10 +6,15 @@ import java.util.Set;
 
 public class ComponentMembrane {
 
+	private final Component component;
+
 	private final HashMap<Class<? extends Event>, Channel> channels;
 
-	public ComponentMembrane(Map<Class<? extends Event>, Channel> map) {
+	public ComponentMembrane(Component component,
+			Map<Class<? extends Event>, Channel> map) {
+		this.component = component;
 		channels = new HashMap<Class<? extends Event>, Channel>();
+
 		Set<Map.Entry<Class<? extends Event>, Channel>> set = map.entrySet();
 		for (Map.Entry<Class<? extends Event>, Channel> entry : set) {
 			channels.put(entry.getKey(), entry.getValue());
@@ -18,5 +23,9 @@ public class ComponentMembrane {
 
 	public Channel getChannel(Class<? extends Event> eventType) {
 		return channels.get(eventType);
+	}
+
+	public Component getComponent() {
+		return component;
 	}
 }
