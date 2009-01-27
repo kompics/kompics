9e60595db0966ef2b80d432e3df0fffa643fd6b3
Cosmin Arad <cosmin@sics.se>
2009-01-27 20:02:41 +0000

    preparing 0.4

--- management/src/main/java/se/sics/kompics/management/jconsole/EventTableModel.java
+++
@@ -1,8 +0,0 @@
-package se.sics.kompics.management.jconsole;
-
-import javax.swing.table.DefaultTableModel;
-
-@SuppressWarnings("serial")
-public class EventTableModel extends DefaultTableModel {
-
-}
