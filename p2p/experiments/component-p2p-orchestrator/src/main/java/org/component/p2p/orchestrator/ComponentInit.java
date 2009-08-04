79da0d1b6f9f6b7d55bad31455d4091ca5c27a45
Cosmin Arad <cosmin@sics.se>
2009-08-04 19:15:24 +0000

    code restructuring in progress

---
+++ p2p/experiments/component-p2p-orchestrator/src/main/java/org/component/p2p/orchestrator/ComponentInit.java
@@ -0,0 +1,16 @@
+package org.component.p2p.orchestrator;
+
+import se.sics.kompics.Init;
+
+public final class ComponentInit extends Init {
+
+	private final int attribute;
+
+	public ComponentInit(int attribute) {
+		this.attribute = attribute;
+	}
+	
+	public int getAttribute() {
+		return attribute;
+	}
+}
