1b71e84179c4107f92d9589d562a0c8b0959187e
Cosmin Arad <cosmin@sics.se>
2009-08-04 14:09:55 +0000

    code restructuring in progress

---
+++ p2p/component-bootstrap-server/src/main/java/org/component/bootstrap/server/Component.java
@@ -0,0 +1,45 @@
+package org.component.bootstrap.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.sics.kompics.ComponentDefinition;
+import se.sics.kompics.Handler;
+import se.sics.kompics.Positive;
+import se.sics.kompics.network.Network;
+import se.sics.kompics.timer.Timer;
+
+public final class Component extends ComponentDefinition {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(Component.class);
+
+	private Positive<Network> network = positive(Network.class);
+	private Positive<Timer> timer = positive(Timer.class);
+
+	int state;
+
+	public Component() {
+		subscribe(handleInit, control);
+		subscribe(handleMessage, network);
+		subscribe(handleTimeout, timer);
+	}
+
+	private Handler<ComponentInit> handleInit = new Handler<ComponentInit>() {
+		public void handle(ComponentInit init) {
+			state = init.getAttribute();
+		}
+	};
+
+	private Handler<ComponentMessage> handleMessage = new Handler<ComponentMessage>() {
+		public void handle(ComponentMessage message) {
+			logger.info("Message received");
+		}
+	};
+
+	private Handler<ComponentTimeout> handleTimeout = new Handler<ComponentTimeout>() {
+		public void handle(ComponentTimeout timeout) {
+			logger.info("Timeout received");
+		}
+	};
+}
---
+++ p2p/port-bootstrap/src/main/java/org/port/bootstrap/Port.java
@@ -0,0 +1,10 @@
+package org.port.bootstrap;
+
+import se.sics.kompics.PortType;
+
+public final class Port extends PortType {
+	{
+		positive(EventOut.class);
+		negative(EventIn.class);
+	}
+}
