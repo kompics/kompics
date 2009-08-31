003c3c89432f35f15da2704627caab34e8049c0e
Cosmin Arad <cosmin@sics.se>
2009-08-31 08:49:47 +0000

    added ChordMonitorClient

---
+++ p2p/systems/chord/component-chord-monitor-client/src/main/java/se/sics/kompics/p2p/monitor/chord/client/ChordMonitorClientInit.java
@@ -0,0 +1,53 @@
+/**
+ * This file is part of the Kompics P2P Framework.
+ * 
+ * Copyright (C) 2009 Swedish Institute of Computer Science (SICS)
+ * Copyright (C) 2009 Royal Institute of Technology (KTH)
+ *
+ * Kompics is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package se.sics.kompics.p2p.monitor.chord.client;
+
+import se.sics.kompics.Init;
+import se.sics.kompics.address.Address;
+import se.sics.kompics.p2p.monitor.chord.server.ChordMonitorConfiguration;
+
+/**
+ * The <code>ChordMonitorClientInit</code> class.
+ * 
+ * @author Cosmin Arad <cosmin@sics.se>
+ * @version $Id$
+ */
+public final class ChordMonitorClientInit extends Init {
+
+	private final ChordMonitorConfiguration configuration;
+
+	private final Address self;
+
+	public ChordMonitorClientInit(ChordMonitorConfiguration configuration,
+			Address self) {
+		super();
+		this.configuration = configuration;
+		this.self = self;
+	}
+
+	public ChordMonitorConfiguration getConfiguration() {
+		return configuration;
+	}
+
+	public final Address getSelf() {
+		return self;
+	}
+}
