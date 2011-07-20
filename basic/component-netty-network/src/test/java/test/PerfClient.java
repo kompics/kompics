1371151deccd67fae29bfcb61cb54f4ae827a0da
Cosmin Arad <cosmin@sics.se>
2011-07-20 10:29:47 +0000

    added Perf Client/Server

---
+++ basic/component-netty-network/src/test/java/test/PerfClient.java
@@ -0,0 +1,80 @@
+package test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import se.sics.kompics.Component;
+import se.sics.kompics.ComponentDefinition;
+import se.sics.kompics.Handler;
+import se.sics.kompics.Kompics;
+import se.sics.kompics.Start;
+import se.sics.kompics.address.Address;
+import se.sics.kompics.network.Network;
+import se.sics.kompics.network.netty.NettyNetwork;
+import se.sics.kompics.network.netty.NettyNetworkInit;
+
+public class PerfClient extends ComponentDefinition {
+
+	public static void main(String[] args) {
+		Kompics.createAndStart(PerfClient.class);
+	}
+
+	Address c = new Address(InetAddress.getLocalHost(), 3333, 0);
+	Address s = new Address(InetAddress.getLocalHost(), 2222, 0);
+	Address mcast;
+
+	Component grizzly;
+	long startTime, received = 0, lastTime, lastCount = 0;
+
+	int pipeline = 20;
+	
+	public PerfClient() throws UnknownHostException {
+		grizzly = create(NettyNetwork.class);
+		subscribe(start, control);
+		subscribe(h, grizzly.provided(Network.class));
+		trigger(new NettyNetworkInit(c, 3, 0), grizzly.control());
+	}
+
+	Handler<Start> start = new Handler<Start>() {
+		public void handle(Start event) {
+			String message = "Hello!";
+			
+			for (int i = 0; i < pipeline; i++) {
+				TestMessage tm = new TestMessage(c, s, message.getBytes());
+				trigger(tm, grizzly.provided(Network.class));
+			}
+			startTime = System.nanoTime();
+			lastTime = System.nanoTime();
+		}
+	};
+
+	Handler<TestMessage> h = new Handler<TestMessage>() {
+		public void handle(TestMessage event) {
+			received++;
+			lastCount++;
+
+			if (received % 10000 == 0)
+				stats();
+
+			trigger(new TestMessage(event.getDestination(), event.getSource(),
+					"Hello".getBytes()), grizzly.provided(Network.class));
+		}
+	};
+
+	void stats() {
+		long now = System.nanoTime();
+		
+		dumpStats("Last 10000 ", now - lastTime, lastCount);
+		dumpStats("Overall                        ", now - startTime, received);
+		
+		lastTime = System.nanoTime();
+		lastCount = 0;
+	}
+	
+	void dumpStats(String stat, long time, long count) {
+		double tput = (double) count;
+		double seconds = ((double) time) / 1000000000.0;
+		tput /= seconds;
+		System.out.println(stat + String.format("%.3f", tput) + " msgs/s");
+	}
+}
---
+++ basic/component-netty-network/src/test/java/test/PerfServer.java
@@ -0,0 +1,38 @@
+package test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import se.sics.kompics.Component;
+import se.sics.kompics.ComponentDefinition;
+import se.sics.kompics.Handler;
+import se.sics.kompics.Kompics;
+import se.sics.kompics.address.Address;
+import se.sics.kompics.network.Network;
+import se.sics.kompics.network.netty.NettyNetwork;
+import se.sics.kompics.network.netty.NettyNetworkInit;
+
+public class PerfServer extends ComponentDefinition {
+
+	public static void main(String[] args) {
+		Kompics.createAndStart(PerfServer.class);
+	}
+
+	Component grizzly;
+	Address self;
+
+	public PerfServer() throws UnknownHostException {
+		grizzly = create(NettyNetwork.class);
+		subscribe(h, grizzly.provided(Network.class));
+		self = new Address(InetAddress.getLocalHost(), 2222, 0);
+
+		trigger(new NettyNetworkInit(self, 3, 0), grizzly.control());
+	}
+
+	Handler<TestMessage> h = new Handler<TestMessage>() {
+		public void handle(TestMessage event) {
+			trigger(new TestMessage(self, event.getSource(), "World".getBytes()),
+					grizzly.provided(Network.class));
+		}
+	};
+}
