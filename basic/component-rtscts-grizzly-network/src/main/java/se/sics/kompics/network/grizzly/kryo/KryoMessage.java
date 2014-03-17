62cf595889cebc73a0046c2df5332957f3da08b3
Lars Kroll <lkroll@sics.se>
2014-03-17 17:14:41 +0100

    fixed a bug with grizzly network Inet4Address serialization on openJDK

--- basic/component-rtscts-grizzly-network/src/main/java/se/sics/kompics/network/grizzly/kryo/KryoMessage.java
+++ basic/component-rtscts-grizzly-network/src/main/java/se/sics/kompics/network/grizzly/kryo/KryoMessage.java
@@ -1,5 +1,6 @@
 package se.sics.kompics.network.grizzly.kryo;
 
+import java.net.InetAddress;
 import java.net.Inet4Address;
 import java.nio.ByteBuffer;
 import java.util.LinkedList;
@@ -56,7 +57,58 @@ public class KryoMessage {
 			}
 		});
                 //ImmutableCollectionsSerializer.registerSerializers(kryo);
-		kryo.register(Inet4Address.class);
+		kryo.register(InetAddress.class, new SimpleSerializer<InetAddress>() {
+			@Override
+			public InetAddress read(ByteBuffer buffer) {
+				
+				byte[] adrbytes = new byte[4];
+				buffer.get(adrbytes);
+				try {
+					InetAddress adr = InetAddress.getByAddress(adrbytes);
+					//System.out.println("Used InetDeSerialiser for " + adr.getHostAddress());
+					return adr;
+				} catch(Exception ex) {
+					throw new RuntimeException(ex);
+				}
+
+			}
+
+			@Override
+			public void write(ByteBuffer buffer, InetAddress adr) {
+				//System.out.println("Using InetSerialiser for " + adr.getHostAddress());
+				byte[] adrbytes = adr.getAddress();
+				if (adrbytes.length != 4) {
+					throw new RuntimeException("Address " + adr.getHostAddress() + " has a weird format ("+adrbytes.length+") : " + adrbytes);
+				}
+				buffer.put(adrbytes);
+			}
+		});
+		kryo.register(Inet4Address.class, new SimpleSerializer<Inet4Address>() {
+			@Override
+			public Inet4Address read(ByteBuffer buffer) {
+				
+				byte[] adrbytes = new byte[4];
+				buffer.get(adrbytes);
+				try {
+					Inet4Address adr = (Inet4Address) Inet4Address.getByAddress(adrbytes);
+					//System.out.println("Used Inet4DeSerialiser for " + adr.getHostAddress());
+					return adr;
+				} catch(Exception ex) {
+					throw new RuntimeException(ex);
+				}
+
+			}
+
+			@Override
+			public void write(ByteBuffer buffer, Inet4Address adr) {
+				//System.out.println("Using Inet4Serialiser for " + adr.getHostAddress());
+				byte[] adrbytes = adr.getAddress();
+				if (adrbytes.length != 4) {
+					throw new RuntimeException("Address " + adr.getHostAddress() + " has a weird format ("+adrbytes.length+") : " + adrbytes);
+				}
+				buffer.put(adrbytes);
+			}
+		});
 		kryo.register(Address.class);
 		if (compress) {
 			kryo.register(Message.class, new DeflateCompressor(
