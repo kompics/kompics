9a28d2cb76a13db5f6a58345035b71478983d592
ifeanyi <ify1992@yahoo.com>
2017-04-02 15:29:26 +0200

    make Unsafe abstract

--- core/src/main/java/se/sics/kompics/Unsafe.java
+++ core/src/main/java/se/sics/kompics/Unsafe.java
@@ -3,21 +3,22 @@ package se.sics.kompics;
 import java.util.Collection;
 import java.util.Map;
 
-public class Unsafe {
+public abstract class Unsafe {
 
   public static Map<Class<? extends PortType>, JavaPort<? extends PortType>> getPositivePorts(Component component) {
-    return ((JavaComponent) component).getPositivePorts();
+      return ((JavaComponent) component).getPositivePorts();
   }
 
   public static Map<Class<? extends PortType>, JavaPort<? extends PortType>> getNegativePorts(Component component) {
-    return ((JavaComponent) component).getNegativePorts();
+      return ((JavaComponent) component).getNegativePorts();
   }
 
   public static Collection<Class<? extends KompicsEvent>> getPositiveEvents(PortType portType) {
-    return portType.getPositiveEvents();
+      return portType.getPositiveEvents();
   }
 
   public static Collection<Class<? extends KompicsEvent>> getNegativeEvents(PortType portType) {
-    return portType.getNegativeEvents();
+      return portType.getNegativeEvents();
   }
 }
+
